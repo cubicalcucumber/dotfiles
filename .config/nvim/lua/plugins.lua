1bba9b91f3510ffab0019162a93d7b2516c8d4c4
Johannes Stäbler <johannes.markus.staebler@gmail.com>
2024-03-30 21:14:14 +0100

    nvim-config: Add treesitter

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -106,6 +106,73 @@ require("lazy").setup {
       },
     },
   },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    event = { "VeryLazy" },
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter-textobjects",
+    },
+    build = ":TSUpdate",
+
+    opts = {
+      ensure_installed = { "c", "lua", "luadoc", "markdown", "markdown_inline", "ruby", "vimdoc" },
+      auto_install = true,
+
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = { "ruby" },
+      },
+      indent = { enable = true, disable = { "ruby" } },
+
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<C-space>",
+          node_incremental = "<C-space>",
+          scope_incremental = false,
+          node_decremental = "<bs>",
+        },
+      },
+
+      textobjects = {
+        select = {
+          enable = true,
+          keymaps = {
+            ["af"] = "@function.outer",
+            ["if"] = "@function.inner",
+            ["ac"] = "@class.outer",
+            ["ic"] = "@class.inner",
+          },
+        },
+        move = {
+          enable = true,
+          lookahead = true,
+          goto_next_start = {
+            ["]]"] = "@function.outer",
+            ["]c"] = "@class.outer",
+            ["]b"] = "@block.inner",
+            ["]p"] = "@parameter.inner",
+          },
+          goto_previous_start = {
+            ["[["] = "@function.outer",
+            ["[c"] = "@class.outer",
+            ["[b"] = "@block.inner",
+            ["[p"] = "@parameter.inner",
+          },
+        },
+        swap = {
+          enable = true,
+          swap_next = { ["<leader>x"] = "@parameter.inner" },
+          swap_previous = { ["<leader>z"] = "@parameter.inner" },
+        },
+      },
+    },
+
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
+    end,
+  },
 }
 
 vim.g.vimwiki_list = {

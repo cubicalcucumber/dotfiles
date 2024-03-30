0bb049ea80412797fb897d9ef3f41a8c37c51e4e
Johannes Stäbler <johannes.markus.staebler@gmail.com>
2024-03-30 13:13:50 +0100

    nvim-config: Change to lazy.nvim

--- .config/nvim/plugin/plugins.lua
+++ .config/nvim/plugin/plugins.lua
@@ -1,5 +1,12 @@
-require "paq" {
-  "savq/paq-nvim",
+-- Install `lazy.nvim` if not already done
+local lazy_path = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazy_path) then
+  local repo = "https://github.com/folke/lazy.nvim.git"
+  vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazy_path }
+end
+vim.opt.rtp:prepend(lazy_path)
+
+require("lazy").setup {
   "neovim/nvim-lspconfig",
 
   "nvim-lua/plenary.nvim",

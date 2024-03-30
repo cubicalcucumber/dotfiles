ba66c3c93f915098e9625d0129046bb6a5d3d934
Johannes Stäbler <johannes.markus.staebler@gmail.com>
2024-03-30 15:22:59 +0100

    nvim-config: Update a few settings

--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -1,62 +1,58 @@
--------------
--- Editing --
--------------
-
 function set_tab_width(width)
   vim.opt.tabstop     = width
   vim.opt.shiftwidth  = width
   vim.opt.softtabstop = width
 end
 
--- Use 2 spaces instead of a tab.
+-- Use 2 spaces instead of a tab
 set_tab_width(2)
 vim.opt.expandtab = true
 
--- Make all yank/delete operations copy to the clipboard.
+-- Make all yank/delete operations copy to the clipboard
 vim.opt.clipboard = "unnamedplus"
 
-
-----------------------------
--- Display and UI options --
-----------------------------
-
--- Use 24-bit RGB color in the terminal.
+-- Use 24-bit RGB color in the terminal
 vim.opt.termguicolors = true
 
--- Activate the terminal title.
-vim.opt.title = true
-
--- Always show the status line.
+-- Always show the status line
 vim.opt.laststatus = 2
 
--- Use relative line numbers.
+-- Use relative line numbers
 vim.opt.relativenumber = true
 vim.opt.number = true
 
--- Highlight the current line.
+-- Highlight the current line
 vim.opt.cursorline = true
 
--- Show trailing spaces and tab characters.
+-- Keep 10 lines above/below the cursor when scrolling
+vim.opt.scrolloff = 10
+
+-- Show trailing spaces and tab characters
 vim.opt.listchars = { trail = "·", tab = "> " }
 vim.opt.list = true
 
--- Set the vertical separator.
+-- Set the vertical separator
 vim.opt.fillchars = { vert = "|" }
 
--- Indicate wrapped lines.
-vim.opt.showbreak = " ~> "
+-- Set up line breaks
+vim.opt.breakindent = true
 
--- Break on word boundaries.
-vim.opt.linebreak = true
-
--- Turn on search highlighting.
+-- Turn on search highlighting
 vim.opt.incsearch = true
 vim.opt.hlsearch = true
 
+-- Only wait 250ms for a mapped sequence to complete
+vim.opt.timeoutlen = 250
 
----------------------
--- Various options --
----------------------
+-- Enable mouse support
+vim.opt.mouse = "a"
 
--- Don't use swap files.
+-- Splits
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- Don't use swap files
 vim.opt.swapfile = false
+
+-- Remember the undo history
+vim.o.undofile = true

3c0fab491d4908a1f47cd95cf316b6de28041f33
Johannes Stäbler <johannes.markus.staebler@gmail.com>
2024-04-03 23:45:42 +0200

    nvim-config: Add autocommand for highlighting yanked text

--- .config/nvim/lua/autocommands.lua
+++ .config/nvim/lua/autocommands.lua
@@ -1,8 +1,18 @@
+-- Resize all splits when resizing Vim
 vim.api.nvim_create_autocmd("VimResized", {
   group = vim.api.nvim_create_augroup("AutoResizeSplits", { clear = true }),
   pattern = "*",
   callback = function()
     local keys = vim.api.nvim_replace_termcodes("<c-w>=", true, false, true)
     vim.api.nvim_feedkeys(keys, "n", false)
+  end,
+})
+
+-- Highlight yanked text
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = vim.api.nvim_create_augroup("YankHighlightg", { clear = true }),
+  pattern = "*",
+  callback = function()
+    vim.highlight.on_yank({ timeout = 200 })
   end,
 })

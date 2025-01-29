318012db02775186e194e89078d185a828d7001d
Johannes Stäbler <johannes.markus.staebler@gmail.com>
2025-01-29 23:22:03 +0100

    nvim: Add missing chadrc.lua

---
+++ .config/nvim/lua/chadrc.lua
@@ -0,0 +1,22 @@
+return {
+  base46 = { theme = "catppuccin", },
+
+  ui = {
+    statusline = {
+        enabled = true,
+        theme = "vscode_colored",
+    },
+
+    telescope = { style = "bordered" },
+    tabufline = { enabled = false },
+  },
+
+  term = {
+    float = {
+      row = 0.25,
+      col = 0.25,
+      width = 0.5,
+      height = 0.5,
+    },
+  },
+}
